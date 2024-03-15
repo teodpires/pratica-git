54996febde931b255b992a4aa491e35f926b896f
Teo Durante Pires <2021305232@farroupilha.ifrs.edu.br>
2024-03-15 09:34:58 -0300

    Inserindo labels para os eixos horizontal e vertical

--- geragrafico.py
+++ geragrafico.py
@@ -1,9 +1,12 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
 from leitorarquivo import LeitorArquivo
 
 def main():
     leitor = LeitorArquivo('data.txt')
     valores = leitor.getValores()
     print(valores)
-
-
+    plt.plot(valores)
+    plt.show()
 main()
